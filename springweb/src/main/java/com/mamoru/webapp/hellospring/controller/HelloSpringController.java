c2ede7655c5adacca12d72aac5baad5ed4c3ff30
fightingmamoru@gmail.com <ma131026mo*>
2016-03-11 15:15:04 +0900

    Module(springweb) - Add maven dependency for log4j2 and slf4j. And add file "log4j2.xml". Add logger for test.

--- springweb/src/main/java/com/mamoru/webapp/hellospring/controller/HelloSpringController.java
+++ springweb/src/main/java/com/mamoru/webapp/hellospring/controller/HelloSpringController.java
@@ -1,5 +1,7 @@
 package com.mamoru.webapp.hellospring.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,17 +9,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloSpringController
 {
+	private static final Logger LOGGER = LogManager.getLogger(HelloSpringController.class);
+
 	private static final String JSP_PATH = "hellospring/";
 
 	@RequestMapping(value = "/helloSpring")
 	public ModelAndView hello()
 	{
+		LOGGER.debug("[START hello()]======================================================================");
+
 		ModelAndView mav = new ModelAndView();
 
 		mav.setViewName(JSP_PATH + "hellospring");
 
 		mav.addObject("title", "[Spring Web MVC]");
 		mav.addObject("message", "Hello Spring!");
+
+		LOGGER.debug("[END hello()]=======================================================================");
 
 		return mav;
 	}
